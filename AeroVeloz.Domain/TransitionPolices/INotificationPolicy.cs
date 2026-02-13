f43810c763b7686187cbc3870da282abcac1eac8
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-02-13 18:08:44 -0400

    fead: creacion de los contractos de la capa de dominio y metodos de los mismos

---
+++ AeroVeloz.Domain/TransitionPolices/INotificationPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AeroVeloz.Domain.TransitionPolices
+{
+    public interface INotificationPolicy
+    {
+
+        //descomentar cuando se cree la clase vuelos y el enum de operationalChange
+        public bool ShouldNotify(/*OperationalChange change, Flight: flight */);
+    }
+}
