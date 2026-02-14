10a2a929c858ee80878045499a572768a1a45e3e
Adrixn <hxdes@DESKTOP-CROAITG>
2026-02-13 21:13:17 -0400

    Scaffolding de BD, Generacion de entidades, separación a la capa Domain y conexión segura de base de datos

---
+++ AeroVeloz.Domain/Entities/FlightHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AeroVeloz.Domain.Entities;
+public partial class FlightHistory
+{
+    public Guid HistoryId { get; set; }
+
+    public Guid FlightId { get; set; }
+
+    public Guid? OldStateId { get; set; }
+
+    public Guid? NewStateId { get; set; }
+
+    public DateTime? ChangeDate { get; set; }
+
+    public Guid? ChangeBy { get; set; }
+
+    public virtual Flight Flight { get; set; } = null!;
+}
