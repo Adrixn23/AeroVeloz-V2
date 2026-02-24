c537d9f053d345232786834f8d20391b6ad58a74
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-24 19:12:19 -0400

    fead: creacion de eventos de security operational

---
+++ Core/AeroVeloz.Domain/Events/Operations/SecurityOperationEvents/OperationalDelayDetectedEvent.cs
@@ -0,0 +1,20 @@
+﻿using AeroVeloz.Domain.Entities.Flights;
+using MediatR;
+
+namespace AeroVeloz.Domain.Events.Operations.SecurityOperationEvents
+{
+    public record OperationalDelayDetectedEvent(
+        
+       int flightNumber,
+       string? codeAirline,
+       string? codeAirport,
+       DateTimeOffset OriginalScheduledTime,
+       DateTimeOffset NewScheduledTime,
+       string? cause,
+       //TypeDealy delay,
+       int idUserOperational,
+       List<Flight> AffectedFlightsConnection
+
+        ) : INotification;
+    
+}
