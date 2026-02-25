05541103dc08d813e17929817425fed64f73e781
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-25 16:46:45 -0400

    fead: modificacion de elemenetos operativos orquestador y entidad capa de dominio

--- Core/AeroVeloz.Domain/Entities/Operations/OperationChange.cs
+++ Core/AeroVeloz.Domain/Entities/Operations/OperationChange.cs
@@ -5,7 +5,7 @@ namespace AeroVeloz.Domain.Entities.Operations;
 
 public partial class OperationChange :  BEntity<Guid>
 {
-      public int  idUser { get; private set; }  
+      public Guid  idUser { get; private set; }  
       public OperationalChangeType operationalChangeType { get; private set; }
       public string? codeAirline { get; private set; }
       public int flightNumber { get; private set; }
@@ -18,19 +18,30 @@ public partial class OperationChange :  BEntity<Guid>
        public FlightState newValue {get; private set; } 
 
        */
+
       public DateTime changeAt { get; private set; }
+
       public string? cause { get; private set; }
 
-    private OperationChange(Guid id,int idUser, OperationalChangeType operationalChangeType, string codeAirline, int flightNumber
+    private OperationChange(Guid id, Guid idUser, OperationalChangeType operationalChangeType, string codeAirline, int flightNumber
          /*FlightState previousValue, FlightState newValue */)
     {
         this.Id = id;
         this.operationalChangeType = operationalChangeType;
         this.codeAirline = codeAirline;
         this.flightNumber = flightNumber;
+        this.idUser = idUser;
+
         /*this.previousValue = previosValue;
          this.newValue = newValue;
          */
+    }
+
+    public static OperationChange CreateOperation(Guid id, Guid idUser, OperationalChangeType operationalChangeType, string codeAirline, int flightNumber
+         /*FlightState previousValue, FlightState newValue */)
+    {
+        return new OperationChange(id, idUser, operationalChangeType, codeAirline, flightNumber
+         /*FlightState previousValue, FlightState newValue */);
     }
 
 }
