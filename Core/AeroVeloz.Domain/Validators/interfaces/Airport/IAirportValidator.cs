529e6d2f84298ff52b3fa57856488f3573b5a63a
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-24 23:05:22 -0400

    fead: Creacion del modulo de super admin

--- Core/AeroVeloz.Domain/Validators/interfaces/Airport/IAirportValidator.cs
+++ Core/AeroVeloz.Domain/Validators/interfaces/Airport/IAirportValidator.cs
@@ -1,10 +1,13 @@
 ﻿using AeroVeloz.Domain.Common.ValidationBase;
-using AeroVeloz.Domain.Entities.Organization.Airports;
+using AeroVeloz.Domain.Entities.Airport;
 
 namespace AeroVeloz.Domain.Validators.interfaces.Airports
 {
-    public interface IAirportValidator
-    {
-        ValidationResult Validation(Airport airport);
-    }
+    public interface IAirportValidator { 
+        ValidationResult ValidateAirportRegistration(Airport airport);
+        ValidationResult ValidateAirportCode(string airportCode); 
+        ValidationResult ValidateAirlineConnection(string airportCode, string airlineCode, string apiToken); 
+        ValidationResult ValidateAirportAccess(Guid userId, string airportCode); 
+        ValidationResult ValidateApiKey(string airportCode, string apiKey); 
+        ValidationResult ValidateAirportDeactivation(string airportCode); }
 }
