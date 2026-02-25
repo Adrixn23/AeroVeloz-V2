2117fce0f588803621d8bb90922b6c2ebb2c6e5b
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-25 16:26:17 -0400

    fead: Creacion de evento de los usuarios y creacion de eventos adicionales de consulta en el domain service

---
+++ Core/AeroVeloz.Domain/Events/User/UserCreatedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using AeroVeloz.Domain.Common.Enums;
+
+namespace AeroVeloz.Domain.Events.User
+{
+    public  record UserCreatedDomainEvent(
+        Guid idUser,
+        string? codeAirport,
+        Role Role,
+        DateTime createAt
+        ) : INotification
+    
+}
