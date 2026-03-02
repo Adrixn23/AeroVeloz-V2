0f6382d679bc2c07599fd0d59ab3567f12bccea3
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-03-02 16:36:58 -0400

    fead: modificacion del validator user admin por un validator general de usuarios para mayor facilidad de validacion, colocacion de los diversos elementos de  user code errores para las validaciones y elementos de creacion o validation result de los domain service que permitiran entocnes orquestar todo lo ocurrido dentro de dichas validaciones permitiendo asi entonces devolver mensajes claros y reutilizables en el aplicativo desktop o web segun sea necesario

---
+++ Core/AeroVeloz.Application/DTOs/Users/UserRemove.cs
@@ -0,0 +1,8 @@
+﻿namespace AeroVeloz.Application.DTOs.Users
+{
+    public record  UserRemove
+    {
+        public Guid idUser { get; set; }
+        public bool isActive { get; set; } 
+    }
+}
---
+++ Core/AeroVeloz.Domain/DomainServices/Interfaces/User/IDomainServiceUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AeroVeloz.Domain.DomainServices.Interfaces.User
+{
+    public interface IDomainServiceUser
+    {
+       Task<ValidationResult> Validate
+    
+    }
+}
