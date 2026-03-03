bfdfafa89cfeaeaee12413358c0d86aedd81e718
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-03-03 18:37:50 -0400

    fead:  creacion de user repositorio, domain servie de los usuarios y modificacion del model de usuarios base

--- Core/AeroVeloz.Domain/DomainServices/Interfaces/User/IDomainServiceUser.cs
+++ Core/AeroVeloz.Domain/DomainServices/Interfaces/User/IDomainServiceUser.cs
@@ -1,10 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AeroVeloz.Domain.Common.ValidationBase;
+using AeroVeloz.Domain.Entities.Users.Permission;
+using AeroVeloz.Domain.Entities.Users.Roles;
 
 namespace AeroVeloz.Domain.DomainServices.Interfaces.User
 {
     public interface IDomainServiceUser
     {
-       Task<ValidationResult> Validate
-    
+        Task<ValidationResult> ValidateRoleAssignment(Domain.Entities.Users.User user, Roles role, int orgId);
+        Task<ValidationResult> ValidateUserActivation(Domain.Entities.Users.User user);
+        Task<ValidationResult> ValidateUserDeactivation(Domain.Entities.Users.User user);
+        Task<ValidationResult> ValidatePermissionRoleAssignment(Domain.Entities.Users.User user, Roles role, Permission permission);
+        Task<ValidationResult> ValidateOrganizationAssignment(Domain.Entities.Users.User user, int orgId);
     }
 }
--- Core/AeroVeloz.Domain/Models/Users/UserSystemModel.cs
+++ Core/AeroVeloz.Domain/Models/Users/UserSystemModel.cs
@@ -4,10 +4,7 @@
     {
         public Guid userId { get; }
         public string? nameUser { get; }
-        public int idOrganization { get; }
-        public string? typeOrganization { get; }
-        public string? emailOrganization { get; }
         public bool isActiveUser { get; }
-        public bool isActiveOrganization { get; }
+   
     }
 }
