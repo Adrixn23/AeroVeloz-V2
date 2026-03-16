fb1d30befca39b873115879827ea3786d36c0af2
Adrian Francisco Brito Nelkitts <AdrixnDeveloper29@gmail.com>
2026-03-16 00:26:27 -0400

    Actualizacion 16 de marzo

---
+++ Api/AeroVeloz.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using AeroVeloz.Application.DTOs.Users;
+using Microsoft.AspNetCore.Mvc;
+using AeroVeloz.Application.Contracts.Auth;
+
+
+namespace AeroVelozDesktop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public readonly IAuthenticationServicie _authenticationServicie;
+
+        public AuthController(IAuthenticationServicie authenticationServicie)
+        {
+
+            _authenticationServicie = authenticationServicie;
+        }
+
+        // POST api/<AuthController>
+        [HttpPost("{LoginAction}")]
+        public async Task<IActionResult> LoginAction(UserLoginDto userLoginDto)
+        {
+            var result = await _authenticationServicie.LoginAsync(userLoginDto);
+            if (result.Success) return Ok(result);
+            return BadRequest(result);
+        }
+
+    }
+}
